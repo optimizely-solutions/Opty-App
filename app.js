435d8e7c6116200c03cc5ef67294011a002206e5
Kathryn Castle <kathryn@optimizely.com>
2016-06-24 13:49:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -100,23 +100,11 @@
                 type: "mention",
                 id: this.SDRLeadId
               }, {
-                markupType: "Paragraph",
-                type: "MarkupBegin"
-              }, {
                 type: "text",
                 "text": this.ChatterMessage
               }, {
-                markupType: "Paragraph",
-                type: "MarkupEnd"
-              }, {
-                markupType: "Paragraph",
-                type: "MarkupBegin"
-              }, {
                 type: "text",
-                "text": 'Notification By ' + this.currentUser().name() + ". If you need further information, please follow up with " + this.currentUser().name() + " directly. You can view the customer's Zendesk ticket by clicking the link below."
-              }, {
-                markupType: "Paragraph",
-                type: "MarkupEnd"
+                "text": ' Notification By ' + this.currentUser().name() + ". If you need further information, please follow up with " + this.currentUser().name() + " directly. You can view the customer's Zendesk ticket by clicking the link below."
               }, {
                 type: "link",
                 url: "https://optimizely.zendesk.com/agent/tickets/" + this.ticket().id()
